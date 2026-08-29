60ec3c731cf283d1db103643da3b58e2e68d210b
sefaecir <ecirsefa@gmail.com>
2026-08-29 11:08:35 +0300

    chore(ios): strip Xcode file headers, disable header template

--- ios/OtpVault/OtpVault/ContentView.swift
+++ ios/OtpVault/OtpVault/ContentView.swift
@@ -1,10 +1,3 @@
-//
-//  ContentView.swift
-//  OtpVault
-//
-//  Created by Sefa Ecir on 28.08.2026.
-//
-
 import SwiftUI
 
 struct ContentView: View {
--- ios/OtpVault/OtpVault/OtpVaultApp.swift
+++ ios/OtpVault/OtpVault/OtpVaultApp.swift
@@ -1,10 +1,3 @@
-//
-//  OtpVaultApp.swift
-//  OtpVault
-//
-//  Created by Sefa Ecir on 28.08.2026.
-//
-
 import SwiftUI
 
 @main
